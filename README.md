edee49fc177bb2ecd3f10b08abc302851adc6aa6
owll <mr.owllers@gmail.com>
2022-07-03 01:54:06 +0530

    green tea

---
+++ README.md
@@ -0,0 +1,25 @@
+# 🍵 `Green Tea`
+
+a simple cli that keeps your screen awake
+
+---
+
+## Installation
+
+```sh
+cargo install green-tea
+```
+
+## Usage
+
+```sh
+green-tea
+```
+
+## Contributing
+
+this is just a small cli, doesn't need a lot of work but if you have an idea for making this more interesting (ig), open an issue or pull request.
+
+---
+
+License: [MIT](./LICENSE)
---
+++ src/main.rs
@@ -0,0 +1,52 @@
+// hello there!
+mod colours;
+use std::{thread, time::Duration};
+use enigo::*;
+
+
+fn main() {
+    // green tea ascii art
+    let green_tea;
+
+    green_tea = format!(
+        "
+        {green}
+  ▄▄▄                                    ▄▄▄▄▄▄▄              
+▄▀   ▀  ▄ ▄▄   ▄▄▄    ▄▄▄   ▄ ▄▄            █     ▄▄▄    ▄▄▄  
+█   ▄▄  █▀  ▀ █▀  █  █▀  █  █▀  █           █    █▀  █  ▀   █ 
+█    █  █     █▀▀▀▀  █▀▀▀▀  █   █           █    █▀▀▀▀  ▄▀▀▀█ 
+ ▀▄▄▄▀  █     ▀█▄▄▀  ▀█▄▄▀  █   █           █    ▀█▄▄▀  ▀▄▄▀█ 
+
+          __________________
+         /                  \\
+        |                    |
+        |\\__________________/|
+        |                    |-----.
+        |         /\\         |-----. \\
+        |        /  \\        |      \\ \\
+        |       / / |        |       | |
+        |       | | |        |       | |
+        |       \\ | /        |      / /
+        |          \\\\        |_____/ /
+        |           \\\\       |______/
+        |____________________|
+        {reset}
+        ",
+        green = colours::green,
+        reset = colours::reset
+    );
+
+    print!("{}\n", green_tea);
+
+    // ask user for duration
+    let mut duration = String::new();
+    println!("Enter duration in minutes: ");
+    std::io::stdin().read_line(&mut duration).expect("Failed to read line");
+    // magic loop
+    loop{
+        let mut enigo = Enigo::new();
+        enigo.key_click(Key::Shift);
+        print!("Pressed shift to keep the screen alive\n");
+        thread::sleep(Duration::from_secs_f64(duration.trim().parse::<f64>().unwrap() * 60.0));
+    }
+}
